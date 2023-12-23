c3e4ed41cac5600aab95ed5d4a5f53eeeec69b8a
kxxt <rsworktech@outlook.com>
2023-12-23 12:18:26 +0800

    Add pfa-firstboot service

--- common.mk
+++ common.mk
@@ -3,10 +3,12 @@ PFA_PATH := vendor/pfa
 # PFA
 PRODUCT_PACKAGES += \
     bsdtar \
-	shell-selector
+	shell-selector \
+	pfa-firstboot
 
 PRODUCT_COPY_FILES += \
-	$(PFA_PATH)/installation-archive.tar.zst:$(TARGET_COPY_OUT_PRODUCT)/pfa-installation-archive.tar.zst
+	$(PFA_PATH)/installation-archive.tar.zst:$(TARGET_COPY_OUT_PRODUCT)/pfa-installation-archive.tar.zst \
+	$(PFA_PATH)/pfa-init/pfa.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/pfa.rc
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
